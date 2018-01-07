2fdf48179ef672f06401fe1181fe71c8239d4c23
tonygod <tonygod@hotmail.com>
2018-01-07 14:50:02 -0800

    Added title, menu, game scenes

---
+++ Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgr : MonoBehaviour {
+
+    private static SceneMgr instance;
+
+    public bool autoLoadScene = false;
+    public string autoLoadSceneName;
+    public float autoLoadTime = 3.0f;
+
+    private void Awake()
+    {
+        if ( instance != null )
+        {
+            Destroy(this);
+            return;
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        if (autoLoadScene)
+        {
+            StartCoroutine(LoadSceneDelayed(autoLoadTime));
+        }
+    }
+
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    IEnumerator LoadSceneDelayed(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        LoadScene(autoLoadSceneName);
+    }
+}
