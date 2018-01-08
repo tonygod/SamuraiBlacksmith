3ebec71b1d965ec107c6542d943635d4cef858fa
Jonathan West <jrcw@outlook.com>
2018-01-07 16:49:08 -0800

    Initial Commit
    Placehlder UI assets for progress bar added.
    Progress bar decrements with time.

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour {
+
+    public Image timer;
+    public float rate;
+
+    void Update()
+    {
+        timer.fillAmount -= (Time.deltaTime / rate);
+    }
+}
