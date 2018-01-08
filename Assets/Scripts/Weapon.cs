bcfa6eb6dd7cb195b92638927a89e7f43c5d0496
tonygod <tonygod@hotmail.com>
2018-01-07 20:52:31 -0800

    Added weapon selection

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "Weapon", order = 1)]
+public class Weapon : ScriptableObject {
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject stage3;
+}
---
+++ Assets/Scripts/WeaponSelector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelector : MonoBehaviour {
+    public Weapon[] weapons;
+
+    public Weapon SelectRandomWeapon()
+    {
+        int i = Random.Range(0, weapons.Length - 1);
+        Debug.Log("Selected weapon: " + weapons[i].name);
+        return weapons[i];
+    }
+}
