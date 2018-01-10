1648e3d020b761f78debec3cac205b86e01262ed
tonygod <tonygod@hotmail.com>
2018-01-10 15:39:25 -0800

    Updated Colorspace and Lighting settings to allow OpenGLES for mobile

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMenu : MonoBehaviour {
+    public GameObject quitButton;
 
-	public void QuitGame()
+    private void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+            quitButton.SetActive(false);
+    }
+
+
+    public void QuitGame()
     {
         Application.Quit();
     }
